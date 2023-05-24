ba7f7e147fb700ac575532b6262aec647d59f4b6
Martyna Sroka <97385709+Marfaell@users.noreply.github.com>
2023-05-24 18:58:07 +0200

    Created js for modal window
    Added possibility to open it by using 3 buttons and close it by using x or area around it

--- script.js
+++ script.js
@@ -11,6 +11,26 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+// overall function to open the modal
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 // if you have only one line of code after for loop you don't have to use {}
-for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+  // here we turned off the hidden function on these classes what allowed us to pop up the modal window and overlay;
+}
+
+// created overall function for turning off the modal window to keep the code dry
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// here we turned back on the hidden function on these classes what allowed us to close down the modal window and overlay
+btnCloseModal.addEventListener('click', closeModal);
+// this is also turning off the modal and overlay by clicking area around the window
+overlay.addEventListener('click', closeModal);
